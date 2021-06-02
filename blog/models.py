ef0a721b2588840e123c886221d41db6c5f97604
Chidiebere Enekwe <chidieberehenry31@gmail.com>
2021-06-02 05:51:10 +0100

    minor upgrade at my blog model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200, null=True)
-    pub_date = models.DateTimeField(null=True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
     image = models.ImageField(upload_to='images/', null=True)
     body = models.TextField(null=True)
 
