673645eeba139086a5433f7eadbe4991bb252af6
Chidi <chidieberehenry31@gmail.com>
2021-06-05 09:53:51 +0200

    major styling edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from blog.views import blog
+# from blog.views import blog
 
 urlpatterns = [
     path('', views.blog, name='blog'),
-    path('<int:blog_id>/', views.detail, name="detail"),
+    path('<int:blog_id>', views.detail, name="detail"),
     path('create/', views.create, name='create'),
 ]
