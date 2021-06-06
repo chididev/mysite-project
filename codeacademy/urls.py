cb73e1dd660bf172448cafcc9242191988ee069d
Chidi <chidieberehenry31@gmail.com>
2021-06-06 12:52:15 +0200

    minor trial

--- codeacademy/urls.py
+++ codeacademy/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('css/', views.css, name='css'),
     path('python/', views.python, name='python'),
     path('django/', views.django, name='django'),
-    path('<int:html_id>', views.html_detail, name="html_detail"),
-    path('<int:css_id>', views.css_detail, name="css_detail"),
-    path('<int:python_id>', views.python_detail, name="python_detail"),
-    path('<int:django_id>', views.django_detail, name="django_detail"),
+    path('<int:html_id>', views.html_detail, name='html_detail'),
+    path('<int:css_id>', views.css_detail, name='css_detail'),
+    path('<int:python_id>', views.python_detail, name='python_detail'),
+    path('<int:django_id>', views.django_detail, name='django_detail'),
 ]
