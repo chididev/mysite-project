28c0036d64f12d63ced66639400c85d2e77f0876
Chidi <chidieberehenry31@gmail.com>
2021-06-06 07:34:54 +0200

    i got my smptp working :) :) :)

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'chidieberehenry31@gmail.com'
-EMAIL_HOST_PASSWORD = 'chidiebere31@'
+EMAIL_HOST_PASSWORD = 'umhpcpzsalszbwgy'
 
 MIDDLEWARE = [
     # DJANGO MIDDLEWARE CONFIGURATIONS
