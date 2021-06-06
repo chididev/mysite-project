2cc099edf915e38a8e360afc5bab1f80483853e6
Chidi <chidieberehenry31@gmail.com>
2021-06-06 09:40:44 +0200

    done with the frontwnd design of our codeacademy. upnext would be to create the necessary models and further the development. :)

--- codeacademy/urls.py
+++ codeacademy/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
-from codeacademy.views import codeacademy
+from codeacademy.views import *
 
 urlpatterns = [
     path('', views.codeacademy, name='codeacademy'),
+    path('html/', views.html, name='html'),
+    path('css/', views.css, name='css'),
+    path('python/', views.python, name='python'),
+    path('django/', views.django, name='django'),
 ]
--- codeacademy/views.py
+++ codeacademy/views.py
@@ -6,3 +6,23 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def codeacademy(request):
     return render(request, 'codeacademy/codeacademy.html')
+
+
+@login_required
+def html(request):
+    return render(request, 'codeacademy/html5.html')
+
+
+@login_required
+def css(request):
+    return render(request, 'codeacademy/css3.html')
+
+
+@login_required
+def python(request):
+    return render(request, 'codeacademy/python.html')
+
+
+@login_required
+def django(request):
+    return render(request, 'codeacademy/django.html')
